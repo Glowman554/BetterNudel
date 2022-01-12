80d2417f0f2d3a89bdae99f1a3412d5207d6b147
Glowman554 <glowman434@gmail.com>
2022-01-12 08:00:34 +0100

    api docs

---
+++ host/js/api_example.js
@@ -0,0 +1,51 @@
+function stop_login(login_id) {
+	return new Promise((resolve, reject) => {
+		fetch("https://x.glowman554.gq/auth/login/stop?login_id=" + login_id).then(response => response.text()).then(response => {
+			resolve(response);
+		});
+	});
+}
+
+function start_login(discord_tag) {
+	return new Promise((resolve, reject) => {
+		fetch("https://x.glowman554.gq/auth/login/start?discord_id=" + discord_tag.replace("#", "%23")).then(response => response.json()).then(response => {
+			resolve(response.id);
+		});
+	});
+}
+
+function status_login(login_id) {
+	return new Promise((resolve, reject) => {
+		fetch("https://x.glowman554.gq/auth/login/status?login_id=" + login_id).then(response => response.json()).then(response => {
+			resolve(response.token);
+		});
+	});
+}
+
+function self_message(message, login_token) {
+	return new Promise((resolve, reject) => {
+		fetch("https://x.glowman554.gq/api/self_message?body=" + encodeURIComponent(message) + "&login_token=" + login_token).then(response => response.text()).then(response => {
+			resolve(response);
+		});
+	});
+}
+
+async function login(discord_tag) {
+	let login_id = await start_login(discord_tag);
+	let token = null;
+	do {
+		token = await status_login(login_id);
+
+		if (token == null) {
+			await new Promise(resolve => setTimeout(resolve, 1000));
+		}
+	} while (token == null);
+
+	return token;
+}
+
+login("Glowman554#4152").then(token => {
+	self_message("Hello, world!", token).then(response => {
+		console.log(response);
+	});
+});
