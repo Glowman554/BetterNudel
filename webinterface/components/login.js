ea1d78627d43316a6552c3db34a345d8c76c250e
Glowman554 <glowman434@gmail.com>
2022-02-01 07:48:30 +0100

    fixed styling

--- webinterface/components/login.js
+++ webinterface/components/login.js
@@ -39,52 +39,44 @@ export default class Login extends React.Component {
 				</div> : null }
 
 				{ this.state.current_stage == 0 ? <div>
-					<div className="row-padding dark-grey padding-64 container">
-						<div className="content">
-							<div className="twothird">
-								<p>Hello and Welcome to the authentication page!</p>
-								<p>I will guide you trough the whole process!</p>
-								<Button onClick={
-									event => {
-										api_request("/api/v2/login/start").then(data => {
-											data = JSON.parse(data);
+					<Card>
+						<p>Hello and Welcome to the authentication page!</p>
+						<p>I will guide you trough the whole process!</p>
+						<Button onClick={
+							event => {
+								api_request("/api/v2/login/start").then(data => {
+									data = JSON.parse(data);
 
-											this.state.auth_text = `-auth ${data.id}`;
+									this.state.auth_text = `-auth ${data.id}`;
 
-											this.state.current_stage = 1;
-											this.setState(this.state);
+									this.state.current_stage = 1;
+									this.setState(this.state);
 
-											this.interval = setInterval(() => {
-												api_request("/api/v2/login/status?login_id=" + data.id).then(data2 => {
-													data2 = JSON.parse(data2);
+									this.interval = setInterval(() => {
+										api_request("/api/v2/login/status?login_id=" + data.id).then(data2 => {
+											data2 = JSON.parse(data2);
 
-													console.log(data2);
+											console.log(data2);
 
-													if (data2.token) {
-														clearInterval(this.interval);
-														localStorage.setItem("token", data2.token);
-														location.reload();
-													}
-												});
-											}, 1000);
+											if (data2.token) {
+												clearInterval(this.interval);
+												localStorage.setItem("token", data2.token);
+												location.reload();
+											}
 										});
-									}
-								}>Next</Button>
-							</div>
-						</div>
-					</div>
+									}, 1000);
+								});
+							}
+						}>Next</Button>
+					</Card>
 				</div> : null }
 
 				{ this.state.current_stage == 1 ? <div>
-					<div className="row-padding dark-grey padding-64 container">
-						<div className="content">
-							<div className="twothird">
-								<p style={{
-									display: "inline"
-								}}>Please send "<code>{this.state.auth_text}</code>" to the bot on your preferred platform!</p>
-							</div>
-						</div>
-					</div>
+					<Card>
+						<p style={{
+							display: "inline"
+						}}>Please send "<code>{this.state.auth_text}</code>" to the bot on your preferred platform!</p>
+					</Card>
 				</div> : null }
 			</div>
 		)
