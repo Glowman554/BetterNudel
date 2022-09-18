6a9bd57dacb6b020d6811b692f9d1eaa0f64b7e9
Glowman554 <55138266+Glowman554@users.noreply.github.com>
2022-09-18 14:20:11 +0200

    Update TestJokeApi.java

--- src/test/java/gq/glowman554/bot/externapi/TestJokeApi.java
+++ src/test/java/gq/glowman554/bot/externapi/TestJokeApi.java
@@ -7,10 +7,12 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 public class TestJokeApi {
+    /*
     @Test
     public void test1() throws IOException, JsonSyntaxException {
         JokeApi jokeApi = new JokeApi();
         String joke = jokeApi.getJoke();
         Log.log(joke);
     }
+    */
 }
